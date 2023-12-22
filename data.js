d40ad58e7f497c8cf480b894267c5a65e4f63027
Tirelo-Mputle <tirelomputle0@gmail.com>
2023-12-22 12:11:28 +0200

    can dynamically update the text and image based on which tab is clicked

--- data.js
+++ data.js
@@ -1,5 +1,17 @@
 export const tabsData = [
-    {title: "history",text:"history text"},
-    {title: "vision",text:"vision text"},
-    {title: "goals",text:"goals text"},
-]
+  {
+    title: "history",
+    text: "history text Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam commodi dolor quo quis quam nam, atque cupiditate optio ad voluptatum, laudantium illum voluptatibus vitae exercitationem quos porro quod itaque voluptatem.",
+    image: "./images/image-1.JPG",
+  },
+  {
+    title: "vision",
+    text: "vision text Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam commodi dolor quo quis quam nam, atque cupiditate optio ad voluptatum, laudantium illum voluptatibus vitae exercitationem quos porro quod itaque voluptatem.",
+    image: "./images/image-2.JPG",
+  },
+  {
+    title: "goals",
+    text: "goals text Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam commodi dolor quo quis quam nam, atque cupiditate optio ad voluptatum, laudantium illum voluptatibus vitae exercitationem quos porro quod itaque voluptatem.",
+    image: "./images/image-3.JPG",
+  },
+];
--- script.js
+++ script.js
@@ -1,15 +1,37 @@
+import { tabsData } from "./data.js";
 const tabs = document.querySelector(".tabs");
 const tabsBtns = document.querySelectorAll(".tab");
+const contentTitle = document.querySelector(".content-title");
+const contentText = document.querySelector(".content-text");
+const image = document.querySelector(".image");
 
-tabs.addEventListener("click", (e) => {
+/**
+ * Changes the color of the tab by adding the chosen class when the tab is clicked.
+ * @param {Event} e
+ */
+const updateChosenTab = (e) => {
   const id = e.target.dataset.tab;
-  tabsBtns.forEach(tab => {
+  tabsBtns.forEach((tab) => {
     //remove chosen class from all the tabs
-    tab.classList.remove("chosen")
+    tab.classList.remove("chosen");
     //add the chosen class to the tab that's clicked
-    if(tab.dataset.tab === id){
-        tab.classList.add("chosen")
-        
+    if (tab.dataset.tab === id) {
+      tab.classList.add("chosen");
     }
   });
+};
+
+const handleContentUpdate = (e) => {
+  const id = e.target.dataset.tab;
+  const content = tabsData.filter((tab) => {
+    return tab.title === id;
+  });
+  const tab=content[0]
+  contentText.textContent = tab.text;
+  contentTitle.textContent = tab.title;
+  image.src = tab.image;
+};
+tabs.addEventListener("click", (e) => {
+  updateChosenTab(e);
+  handleContentUpdate(e);
 });
